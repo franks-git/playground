4de6449911270022561746415ad45e5206022886
Franklin Kaswiner <fkaswine@everis.com>
2020-05-02 13:06:22 +0200

    Fixing some pom.xml's

--- learning/src/main/java/com/kaswiner/learning/LearnApplication.java
+++ learning/src/main/java/com/kaswiner/learning/LearnApplication.java
@@ -1,13 +1,17 @@
 package com.kaswiner.learning;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnApplication {
+public class LearnApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
+	}
+	
+	public void run(String... args) {
 	}
 
 }
---
+++ learning/src/test/java/com/kaswiner/learning/algorithm/DijkstraTest.java
@@ -0,0 +1,17 @@
+package com.kaswiner.learning.algorithm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class DijkstraTest {
+	
+	@Test
+	public void test() {
+		boolean condition = true;
+		
+		assertTrue(condition == true);
+	}
+
+}
